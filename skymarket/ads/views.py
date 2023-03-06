41b04694c666b5650804d4b1d52b0ce178f276c2
Owwwen86 <smirnovkirill@mail.ru>
2023-03-06 23:54:27 +0300

    Curs 6

--- skymarket/ads/views.py
+++ skymarket/ads/views.py
@@ -47,41 +47,6 @@ class AdViewSet(viewsets.ModelViewSet):
         request.data._mutable = True
         request.data["author"] = request.user.pk
         return super().create(request, *args, **kwargs)
-#
-#     def list(self, request, *args, **kwargs):
-#         categories = request.GET.getlist("cat")
-#         if categories:
-#             self.queryset = self.queryset.filter(category_id__in=categories)
-#
-#         text = request.GET.get("text")
-#         if text:
-#             self.queryset = self.queryset.filter(name__icontains=text)
-#
-#         location = request.GET.get("location")
-#         if location:
-#             self.queryset = self.queryset.filter(author__location__name__icontains=location)
-#
-#         price_from = request.GET.get("price_from")
-#         price_to = request.GET.get("price_to")
-#
-#         if price_from:
-#             self.queryset = self.queryset.filter(price__gte=price_from)
-#         if price_to:
-#             self.queryset = self.queryset.filter(price__lte=price_to)
-#
-#         return super().list(request, *args, **kwargs)
-#
-#
-# @method_decorator(csrf_exempt, name='dispatch')
-# class AdImageUpload(UpdateView):
-#     model = Ad
-#     fields = "__all__"
-#
-#     def post(self, request, *args, **kwargs):
-#         self.object = self.get_object()
-#         self.object.image = request.FILES.get("image")
-#         self.object.save()
-#         return JsonResponse({"name": self.object.name, "image": self.object.image.url})
 
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -109,24 +74,10 @@ class CommentViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         return self.permission_list.get(self.action, self.default_permission)
 
-    # @action(detail=True, methods=['post'])
-    # def create(self, request, pk=None):
-    #     user = request.user.pk
-    #     serializer_class = CommentCreateSerializer
-    #     return user
-
-    # def perform_create(self, serializer):
-    #     serializer.save(author=self.request.user, ad=self.kwargs.get("ad_pk"))
-
     def get_queryset(self):
         return Comment.objects.prefetch_related("author").filter(
             ad=self.kwargs["ad_pk"]
         )
-
-    # def create(self, request, *args, **kwargs):
-    #     # request.data._mutable = True
-    #     request.data["user"] = request.user.pk
-    #     return super().create(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, ad_id=self.kwargs.get("ad_pk"))
